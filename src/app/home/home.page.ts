2c9c842674c296d1c85a0aa716f81095e24de676
Ben Thomas <bennyt9000@gmail.com>
2020-01-06 16:01:28 -0500

    Add basic infinite scroll

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,37 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  posts: number[] = [];
+  maxPosts: number = 100;
+  step: number = 5;
+  loadedPosts: number = 0;
+  allPostsLoaded: boolean = false;
 
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.addPosts(this.step);
+  }
+
+  addPosts(number: number) {
+    for (let i = 0; i < number; i++) {
+      this.posts.push(this.posts.length + 1);
+      this.loadedPosts = this.posts.length + 1;
+    }
+  }
+
+  getMorePosts(event) {
+    setTimeout(() => {
+      this.addPosts(this.step);
+      event.target.complete();
+
+      // App logic to determine if all data is loaded
+      // and disable the infinite scroll
+      if (this.loadedPosts > this.maxPosts) {
+        event.target.disabled = true;
+        this.allPostsLoaded = true;
+      }
+    }, 500)
+  }
 }
